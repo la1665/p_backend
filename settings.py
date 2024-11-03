43c9539c2b652d1284db2b42015d63ecfda7f5d2
Amir <amirsazegar16@gmail.com>
2024-11-03 21:17:59 +0330

    changed .env and settings

--- settings.py
+++ settings.py
@@ -7,12 +7,12 @@ class Settings(BaseSettings):
     POSTGRES_DB: str | Any=None
     POSTGRES_HOST: str | Any=None
     POSTGRES_PORT: int=27202
-    SECRET_KEY: str | Any=None
+    SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int=10
-    ALGORITHM: str | Any=None
-    ADMIN_USERNAME: str | Any=None
-    ADMIN_PASSWORD: str | Any=None
-    ADMIN_EMAIL: str | Any=None
+    ALGORITHM: str
+    ADMIN_USERNAME: str
+    ADMIN_PASSWORD: str
+    ADMIN_EMAIL: str
     AUTH_TOKEN: Optional[str] = None
     HMAC_SECRET_KEY: Optional[str] = None
     MINIO_ENDPOINT: str
